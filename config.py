df16204ee9d7f51548bffbaffe6df7b5ef53f65c
Guilherme Varela <guilhermevarela@protonmail.com>
2022-06-03 09:19:46 +0100

    Set config to the best combination and inc steps.

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 """Configuration"""
-ALPHA = 0.5
-BETA = 0.3
-TAU = 5.0
-ZETA = 0.10
-EXPLORE_EPISODES = 5000
-EPISODES = 8000
+ALPHA = 0.1  # ALPHA:
+BETA = 0.05  # BETA:
+TAU = 10.0   # Final TAU
+ZETA = 0.01
+EXPLORE_EPISODES = 8000
+EPISODES = 16000
 EXPLORE = True  # WHETER OR NOT WE USE EXPLORATION
 
 SEED = 1
-BASE_PATH = 'data/00_duo_hyperparameter_search/08_zeta005'
-
+BASE_PATH = 'data/00_duo_hyperparameter_search/10_best_combination_double_runs'
 N_WORKERS = 6
 N_AGENTS = 2
 AGENT_TYPE = 'ActorCriticCentral'
