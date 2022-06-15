9a44f680bf61ec702f798284b92bb33ce3a5fd07
Guilherme Varela <guilhermevarela@protonmail.com>
2022-06-15 10:51:38 +0100

    Bugfix consensus.sh replacements that changed constants.

--- consensus.sh
+++ consensus.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 # Tests consensus but set different CONSENSUS_MAX_EDGES
-sed -i "s/AGENT_TYPE = .*/AGENT_TYPE = 'ActorCriticConsensus'/g" config.py
-sed -i "s/N_AGENTS = .*/N_AGENTS = 2/g" config.py
+sed -i "s/^AGENT_TYPE = .*/AGENT_TYPE = 'ActorCriticConsensus'/g" config.py
+sed -i "s/^N_AGENTS = .*/N_AGENTS = 2/g" config.py
+sed -i "s/^EXPLORE_EPISODES = .*/EXPLORE_EPISODES = 3000/g" config.py
+sed -i "s/^EPISODES = .*/EPISODES = 7500/g" config.py
 for n in 0 1 
 do
   # Replace agent
   dir="data\/0${n}_duo_consensus_0${n}" &&
-  sed -i "s/CONSENSUS_MAX_EDGES = .*/CONSENSUS_MAX_EDGES = $n/g" config.py &&
-  sed -i "s/BASE_PATH = .*/BASE_PATH = '$dir'/g" config.py  &&
+  sed -i "s/^CONSENSUS_MAX_EDGES = .*/CONSENSUS_MAX_EDGES = $n/g" config.py &&
+  sed -i "s/^BASE_PATH = .*/BASE_PATH = '$dir'/g" config.py  &&
   ./pipeline.py
 done
 
-sed -i "s/N_AGENTS = .*/N_AGENTS = 3/g" config.py
-sed -i "s/EXPLORE_EPISODES = .*/EXPLORE_EPISODES = 5000/g" config.py
-sed -i "s/EPISODES = .*/EXPLORE_EPISODES = 25000/g" config.py
+sed -i "s/^N_AGENTS = .*/N_AGENTS = 3/g" config.py
+sed -i "s/^EXPLORE_EPISODES = .*/EXPLORE_EPISODES = 10000/g" config.py
+sed -i "s/^EPISODES = .*/EPISODES = 30000/g" config.py
 for n in 0 1 2 3 
 do
   # Replace agent
   dir="data\/0${n}_trio_consensus_0${n}" &&
-  sed -i "s/CONSENSUS_MAX_EDGES = .*/CONSENSUS_MAX_EDGES = $n/g" config.py &&
-  sed -i "s/BASE_PATH = .*/BASE_PATH = '$dir'/g" config.py && 
+  sed -i "s/^CONSENSUS_MAX_EDGES = .*/CONSENSUS_MAX_EDGES = $n/g" config.py &&
+  sed -i "s/^BASE_PATH = .*/BASE_PATH = '$dir'/g" config.py && 
   ./pipeline.py
 done
