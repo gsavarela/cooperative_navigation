4cad59fdb2a456e186962346ae100519636ef888
Guilherme Varela <guilhermevarela@protonmail.com>
2022-06-21 21:43:01 +0100

    Add non-linear approximation for ActorCriticIndependent

---
+++ independent_learners_torch.py
@@ -0,0 +1,534 @@
+"""One-step ActorCritic For Continuing tasks.
+
+    * Continuing tasks
+    * V function approximation.
+    * Linear function approximation.
+    * Partial observability setting.
+    * Individual rewards.
+
+References:
+-----------
+..[1] Sutton and Barto 2018. "Introduction to Reinforcement
+  Learning 2nd Edition" (pg 333).
+
+..[2] Sutton, R. S., McAllester, D., Singh, S., & Mansour, Y. (2000).
+  "Policy gradient methods for reinforcement learning with function
+  approximation". In Advances in neural information processing
+  systems (NIPS), Denver, CO. Cambridge: MIT Press.
+"""
+from cached_property import cached_property
+import numpy as np
+import torch
+from torch.optim import Optimizer
+
+import config
+from common import Array, Observation, Action, ActionSet, Rewards
+from common import List, make_dirs, Tuple
+from interfaces import AgentInterface, ActorCriticInterface
+from nets import get_optimizer, select_action, ActorNetwork, CriticNetwork
+
+
+loss_func = torch.nn.MSELoss(size_average=False, reduce=False)
+
+
+class ActorCriticIndependent(AgentInterface, ActorCriticInterface):
+    """ActorCritic with Linear function approximation
+
+    Attributes
+    ----------
+    label: str
+        A description for this particular agent.
+
+    task: str
+        A description for the task.
+
+    tau: float
+        The temperature parameter regulating exploration.
+        Bound to interval [1, 100]
+
+    n_players: int
+        The number of players that must cover landmarks.
+
+    n_features: int
+        The number of features for the agents.
+
+    action_set: ActionSet
+        The action space, i.e, the list of all actions
+        at the disposal of the agent.
+
+    alpha: float = 0.9,
+        The learning rate for policy evaluation.
+
+    beta: float = 0.7,
+        The learning rate for policy parametrization.
+
+    zeta: float = 0.1,
+        The weighting on the global average.
+
+    explore_episodes: int = 100,
+        The number of episodes to explore. The temperature
+        falls linearly with the number of explore_episodes.
+
+    explore: bool = False,
+        Wheter of not the agent should take exploratory actions.
+
+    decay: bool = False,
+        Uses exponential decay on epis
+        seed: int = 0,
+
+    Methods
+    -------
+    act(state): Action
+        Select an action based on state
+
+    reset(seed): None
+        Resets seed, updates number of steps.
+
+    update(state, actions, next_reward, next_state): None
+        Learns from policy improvement and policy evalution.
+    """
+
+    fully_observable = False
+    communication = False
+    torch = True
+
+    def __init__(
+        self,
+        n_players: int,
+        n_features: int,
+        action_set: ActionSet,
+        alpha: float = 0.9,
+        beta: float = 0.7,
+        zeta: float = 0.1,
+        explore_episodes: int = 100,
+        explore: bool = False,
+        decay: bool = False,
+        seed: int = 0,
+    ):
+
+        # Attributes
+        self.n_players = n_players
+        self.action_set = action_set
+        self.n_actions = int(np.round(np.power(len(action_set), 1 / n_players), 0))
+        self.n_features = n_features
+
+        # Parameters
+        # The feature are state-value function features, i.e,
+        # the generalize w.r.t the actions.
+        self.cnets = [CriticNetwork(self.n_features) for _ in range(n_players)]
+        self.anets = [
+            ActorNetwork(self.n_features, self.n_actions) for _ in range(n_players)
+        ]
+        self.zeta = zeta
+        self.mu = np.zeros(self.n_players)
+
+        # Loop control
+        self.alpha = 1 if decay else alpha
+        self.beta = 1 if decay else beta
+        self.explore = explore
+        self.decay = decay
+        self.epsilon = 1.0
+        self.decay_count = 1
+        self.epsilon_step = float((100 - config.TAU) / explore_episodes)
+
+        self.step = 0
+        self.episodes = 0
+        self.reset(seed=seed)
+
+    @cached_property
+    def aopts(self) -> List[Optimizer]:
+        """Actor optimizer"""
+        return [get_optimizer(_net, self.beta) for _net in self.anets]
+
+    @cached_property
+    def copts(self) -> List[Optimizer]:
+        """Critic optimizer"""
+        return [get_optimizer(_net, self.alpha) for _net in self.anets]
+
+    def act(self, state: Observation) -> Tuple[Action, List[float]]:
+        """Select an action based on state
+
+        Parameters:
+        -----------
+        state: Observation
+            The state of the world according to the player.
+
+        Returns:
+        --------
+        actions: Actions
+            The actions for all players.
+
+        """
+        # for _n, _s in enumerate(state):
+        #     pi = self._PI(_s, _n)
+        #     ret.append(self.rng.choice(self.n_actions, p=pi.flatten()))
+        actions, log_probs = zip(
+            *[select_action(_net, _state) for _state, _net in zip(state, self.anets)]
+        )
+        log_probs = [*map(lambda x: x.item(), log_probs)]
+        return tuple(actions), log_probs
+
+    def update(
+        self,
+        state: Observation,
+        actions: Action,
+        next_rewards: Rewards,
+        next_state: Observation,
+        next_actions: Action,
+        log_probs: List[float]
+    ) -> None:
+        """Learns from policy improvement and policy evalution.
+
+        Parameters:
+        -----------
+        state: Observation
+            The state of the world according to the player, at t.
+
+        actions: Action
+            The action for every player, selected at t.
+
+        next_rewards: Rewards
+            The reward earned for each player.
+
+        next_state: Observation
+            The state of the world according to the player, at t + 1.
+
+        next_actions: Action
+            The action for every player, select at t + 1.
+        """
+        # Fully observable setting: stack agents
+        # Convert everything to tensor.
+
+        for _n in range(self.n_players):
+            _s0 = torch.from_numpy(state[_n]).float().to('cpu')
+            _s1 = torch.from_numpy(next_state[_n]).float().to('cpu')
+            _r0 = torch.tensor(next_rewards[_n]).float().to('cpu')
+            _mu = torch.tensor(self.mu[_n]).float().to('cpu')
+            _lp = torch.tensor(log_probs[_n], requires_grad=True)
+
+            # Compute critic loss
+            _v0 = self.cnets[_n](_s0)
+            _v1 = self.cnets[_n](_s1)
+
+            _value_loss = loss_func(_r0 - _mu + _v1, _v0)
+            _value_loss = torch.clamp(_value_loss, min=-1, max=1)**2
+            _value_loss = _value_loss.sum()
+
+            # Compute actor loss
+            _advantage = (_r0 - _mu + _v1 - _v0).clamp(min=-1, max=1)
+            _actor_loss = _lp * _advantage
+
+            # Backpropagate actor
+            self.aopts[_n].zero_grad()
+            _actor_loss.backward(retain_graph=True)
+            self.aopts[_n].step()
+
+            # Backpropagate critic
+            self.copts[_n].zero_grad()
+            _value_loss.backward()
+            self.copts[_n].step()
+            self.mu[_n] += self.zeta * ((_r0 - _mu) + (_v1 - _v0)).clamp(min=-1, max=1).item()
+
+            # self.omega[_n] += self.alpha * delta * _s0
+            # self.omega[_n] = np.clip(self.omega[_n], -1, 1)
+            #
+            # self.theta[_n] += self.beta * delta * self._psi(_s0, actions[_n], _n)
+            #
+            # self.mu[_n] += self.zeta * delta
+            self.step += 1
+
+    # def _psi(self, state: Array, action: int, n: int) -> Array:
+    #     _X = self._X(state)
+    #     _logP = self._logP(state, action, n)
+    #     return (_X * _logP).T
+    #
+    # def _X(self, state: Array) -> Array:
+    #     return np.tile(state / self.tau, (self.n_actions, 1))
+    #
+    # def _logP(self, state: Array, action: int, n: int) -> Array:
+    #     res = -np.tile(self._PI(state, n).T, (1, self.n_features))
+    #     res[action, :] += 1
+    #     return res
+    #
+    # def _PI(self, state: Array, n: int) -> Array:
+    #     ret = softmax(self.theta[n, :].T @ state / self.tau)[None, :]
+    #     return ret
+    #
+    # def _pi(self, state: Array, n: int) -> Array:
+    #     ret = softmax(self.theta[n, :].T @ state)[None, :]
+    #     return ret
+
+
+if __name__ == "__main__":
+    from time import sleep
+    from pathlib import Path
+    import shutil
+
+    import pandas as pd
+    from tqdm.auto import trange
+
+    from environment import Environment
+    from plots import save_frames_as_gif
+    from plots import metrics_plot, returns_plot
+
+    def get_dir():
+        return (
+            Path(config.BASE_PATH)
+            / "03_independent_learners_torch"
+            / "{0:02d}".format(config.SEED)
+        )
+
+    # Some helpful functions
+    def plot_rewards(rewards, episodes) -> None:
+        metrics_plot(
+            rewards,
+            "Average Rewards",
+            "Train Rollouts (N={0}, seed={1:02d})".format(config.N_AGENTS, seed),
+            save_directory_path=get_dir(),
+            episodes=episodes,
+        )
+
+    def plot_mus(mus, episodes) -> None:
+        metrics_plot(
+            mus,
+            "mu",
+            "Train Mu (N={0}, seed={1:02d})".format(config.N_AGENTS, seed),
+            rollouts=False,
+            save_directory_path=get_dir(),
+            episodes=episodes,
+        )
+
+    def plot_returns(rewards, episodes) -> None:
+        returns_plot(
+            rewards,
+            episodes,
+            "Train Returns (N={0}, seed={1:02d})".format(config.N_AGENTS, seed),
+            save_directory_path=get_dir(),
+        )
+
+    def plot_eval(rewards) -> None:
+        metrics_plot(
+            rewards,
+            "Average Rewards",
+            "Evaluation Rollouts (N={0}, seed={1:02d})".format(config.N_AGENTS, seed),
+            save_directory_path=get_dir(),
+            episodes=[],
+        )
+
+    def plot_eval_best(rewards) -> None:
+        metrics_plot(
+            rewards,
+            "Average Rewards",
+            "Evaluation Rollouts (N={0}, seed={1:02d})".format(config.N_AGENTS, seed),
+            save_directory_path=get_dir() / "best",
+        )
+
+    def save(data, filename) -> None:
+        csvname = "{0}.csv".format(filename)
+        file_path = Path(get_dir()) / csvname
+        pd.DataFrame(data=data.round(2)).to_csv(file_path.as_posix(), sep=",")
+
+    seed = config.SEED
+    env = Environment(
+        n=config.N_AGENTS,
+        scenario="networked_spread",
+        seed=seed,
+        central=ActorCriticIndependent.fully_observable,
+        randomize_reward_coefficients=config.RANDOMIZE_REWARD_COEFFICIENTS,
+    )
+    agent = ActorCriticIndependent(
+        n_players=config.N_AGENTS,
+        n_features=env.n_features,
+        action_set=env.action_set,
+        alpha=config.ALPHA,
+        beta=config.BETA,
+        zeta=config.ZETA,
+        explore_episodes=config.EXPLORE_EPISODES,
+        explore=config.EXPLORE,
+        decay=False,
+        seed=config.SEED,
+    )
+
+    print("Fully observable: {0}".format(agent.fully_observable))
+    print("Fully observable: {0}".format(ActorCriticIndependent.fully_observable))
+    print(agent.label)
+
+    make_dirs(get_dir())
+    episodes = []
+    rewards = []
+    mus = []
+    best_rewards = -np.inf
+    best_episode = 0
+    for episode in trange(config.EPISODES, desc="episodes"):
+        # execution loop
+        obs = env.reset()
+        actions, log_probs = agent.act(obs)
+
+        for _ in trange(100, desc="timesteps"):
+            # step environment
+            next_obs, next_rewards, _ = env.step(actions)
+
+            # actor parameters.
+            next_actions, next_log_probs = agent.act(next_obs)
+
+            agent.update(obs, actions, next_rewards, next_obs, next_actions, log_probs)
+
+            obs = next_obs
+            actions = next_actions
+            log_prob = next_log_probs
+
+            rewards.append(np.mean(next_rewards))
+            episodes.append(episode)
+            mus.append(np.mean(agent.mu))
+
+        if episode % 1000 == 0 or episode == config.EPISODES - 1:
+            env.save_checkpoints(get_dir(), "current")
+            agent.save_checkpoints(get_dir(), "current")
+
+            eval_agent = ActorCriticIndependent.load_checkpoint(get_dir(), "current")
+            eval_env = Environment.load_checkpoint(get_dir(), "current")
+
+            eval_rewards = 0
+            first = True
+            for _ in trange(32, desc="evaluation"):
+
+                # Reseeds a copy of the original environment.
+                eval_seed = 47 if first else None
+                obs = eval_env.reset(seed=eval_seed)
+                eval_agent.reset(seed=eval_seed)
+                actions, _ = eval_agent.act(obs)
+                first = False
+                for _ in trange(100, desc="timesteps"):
+                    # step environment
+                    next_obs, next_rewards, _ = env.step(actions)
+
+                    # actor parameters.
+                    next_actions, _ = eval_agent.act(next_obs)
+
+                    obs = next_obs
+                    actions = next_actions
+                    eval_rewards += np.mean(next_rewards)
+
+            print(
+                "Evaluation: Current: {0:0.2f}\tBest: {1:0.2f}".format(
+                    eval_rewards / 3200, best_rewards
+                )
+            )
+            if eval_rewards / 3200 > best_rewards:
+                if not (get_dir() / "best").exists():
+                    (get_dir() / "best").mkdir()
+
+                if not (get_dir() / "best" / str(episode)).exists():
+                    (get_dir() / "best" / str(episode)).mkdir()
+
+                for chkpt_path in (get_dir() / "current").glob("*.chkpt"):
+                    if (get_dir() / "best" / str(episode) / chkpt_path.name).exists():
+                        (get_dir() / "best" / str(episode) / chkpt_path.name).unlink()
+                    shutil.move(
+                        chkpt_path.as_posix(),
+                        (get_dir() / "best" / str(episode)).as_posix(),
+                    )
+
+                if best_episode < episode:
+                    shutil.rmtree((get_dir() / "best" / str(best_episode)).as_posix())
+
+                test_agent = ActorCriticIndependent.load_checkpoint(
+                    (get_dir() / "best"), str(episode)
+                )
+
+                best_rewards = eval_rewards / 3200
+                best_episode = episode
+
+    if (get_dir() / "current").exists():
+        shutil.rmtree((get_dir() / "current").as_posix())
+    # Train plots and saves data.
+    plot_returns(rewards, episodes)
+    plot_rewards(rewards, episodes)
+    plot_mus(mus, episodes)
+
+    pd.DataFrame(
+        data=np.array(rewards).reshape((100, config.EPISODES)),
+        columns=[*range(config.EPISODES)],
+    ).to_csv(
+        (Path(get_dir() / "train-seed{0:02d}.csv".format(config.SEED)).as_posix()),
+        sep=",",
+    )
+
+    # This is a validation run.
+    # Guarantees experiments are comparable.
+    obs = env.reset(seed=config.SEED)
+    prev_world = obs
+    print("World state: {0}".format(obs))
+    # agent.reset()
+    # agent.explore = False
+    # agent.reset(seed=config.SEED)
+    actions, _ = agent.act(obs)
+    frames = []
+    rewards = []
+    for _ in trange(100, desc="timesteps"):
+        # env.render()  # for humans
+        sleep(0.1)
+        frames += env.render(mode="rgb_array")  # for saving
+
+        # step environment
+        next_obs, next_rewards, _ = env.step(actions)
+
+        next_actions, _ = agent.act(next_obs)
+
+        obs = next_obs
+        actions = next_actions
+
+        rewards.append(np.mean(next_rewards))
+
+    plot_eval(rewards)
+    pd.DataFrame(data=np.array(rewards).reshape((100, 1)), columns=[1]).to_csv(
+        (Path(get_dir()) / "test-seed{0:02d}.csv".format(config.SEED)).as_posix(),
+        sep=",",
+    )
+    save_frames_as_gif(
+        frames,
+        dir_path=get_dir(),
+        filename="simulation-seed{0:02d}.gif".format(seed),
+    )
+
+    shutil.copy("config.py", Path(get_dir()).as_posix())
+    print("Evaluation rewards (LAST): {0:0.2f}".format(np.sum(rewards)))
+
+    # This is a validation run for best policy.
+    # Guarantees experiments are comparable.
+    obs = env.reset(seed=config.SEED)
+    print("World state: {0}".format(obs))
+    np.testing.assert_almost_equal(obs, prev_world)
+    agent = ActorCriticIndependent.load_checkpoint(
+        (get_dir() / "best"), str(best_episode)
+    )
+    # agent.reset(seed=config.SEED)
+    # agent.explore = False
+    actions, _ = agent.act(obs)
+    frames = []
+    rewards = []
+    for _ in trange(100, desc="timesteps"):
+        # env.render()  # for humans
+        sleep(0.1)
+        frames += env.render(mode="rgb_array")  # for saving
+
+        # step environment
+        next_obs, next_rewards, _ = env.step(actions)
+
+        next_actions, _ = agent.act(next_obs)
+
+        obs = next_obs
+        actions = next_actions
+        rewards.append(np.mean(next_rewards))
+
+    plot_eval_best(rewards)
+    pd.DataFrame(data=np.array(rewards).reshape((100, 1)), columns=[1]).to_csv(
+        (Path(get_dir()) / "test-best-seed{0:02d}.csv".format(config.SEED)).as_posix(),
+        sep=",",
+    )
+    save_frames_as_gif(
+        frames,
+        dir_path=get_dir(),
+        filename="simulation-best-seed{0:02d}.gif".format(seed),
+    )
+    print("Evaluation rewards (BEST): {0:0.2f}".format(np.sum(rewards)))
